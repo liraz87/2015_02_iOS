d76ecbfdf71523c03db21ab82ab1f4ff2e8f7269
E.L <eladlavi@Elads-MacBook-Pro.local>
2015-03-11 14:02:33 +0200

    Alert View

--- Alerts/Alerts/ViewController.swift
+++ Alerts/Alerts/ViewController.swift
@@ -13,12 +13,47 @@ class ViewController: UIViewController {
     var controller: UIAlertController?;
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        controller = UIAlertController(title: "The title", message: "hi dude!", preferredStyle: UIAlertControllerStyle.Alert);
-        let action = UIAlertAction(title: "Done", style: UIAlertActionStyle.Default) { (paramAction:UIAlertAction!) -> Void in
-            println("the done button was tapped");
+        super.viewDidLoad();
+        
+        controller = UIAlertController(title: "Choose how you would like to share this photo", message: "You cannot bring back a deleted photo", preferredStyle: UIAlertControllerStyle.ActionSheet);
+        
+        let actionEmail = UIAlertAction(title: "Via email", style: UIAlertActionStyle.Default) { (paramAction: UIAlertAction!) -> Void in
+            //a code to run when user clicks email
         };
+        
+        let actionImessage = UIAlertAction(title: "Via iMessage", style: UIAlertActionStyle.Default) { (paramAction: UIAlertAction!) -> Void in
+            //send over iMessage
+        };
+        
+        let actionDelete = UIAlertAction(title: "Delete Photo", style: UIAlertActionStyle.Destructive) { (paramAction:UIAlertAction!) -> Void in
+            //delete the photo
+        }
+        
+        controller?.addAction(actionEmail);
+        controller?.addAction(actionImessage);
+        controller?.addAction(actionDelete);
+        
+        /*
+        controller = UIAlertController(title: "The title", message: "hi dude!", preferredStyle: UIAlertControllerStyle.Alert);
+        controller?.addTextFieldWithConfigurationHandler({ (textField: UITextField!) -> Void in
+            textField.placeholder = "XXXXXXXXXXXXX"
+        });
+        */
+        //let action = UIAlertAction(title: "Done", style: UIAlertActionStyle.Default) { (paramAction:UIAlertAction!) -> Void in
+        //    println("the done button was tapped");
+        //};
+        /*
+        let action = UIAlertAction(title: "Next", style: UIAlertActionStyle.Default) { [weak self](paramAction:UIAlertAction!) -> Void in
+            
+            if let textFields = self!.controller?.textFields{
+                let theTextFields = textFields as [UITextField];
+                let phoneNumber = theTextFields[0].text;
+                println("phoneNumber: \(phoneNumber)");
+            }
+            
+        }
         controller?.addAction(action);
+        */
         
     }
 
